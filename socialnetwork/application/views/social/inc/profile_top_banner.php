275dc9e3293bcc3ffb99f78717cf2bd9379b2463
Gobind rai <raigobind2008@gmail.com>
2015-02-03 19:03:03 +0530

    new files

---
+++ socialnetwork/application/views/social/inc/profile_top_banner.php
@@ -0,0 +1,5 @@
+ <div class="cover overlay">
+                <img src="<?php echo base_url()?>site/images/profile-cover.jpg" alt="cover" />
+                <a href="#" class="btn btn-cover"><i class="fa fa-pencil fa-fw"></i></a>
+            </div>
+          
